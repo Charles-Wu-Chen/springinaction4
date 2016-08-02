04b0dc5e69584cb5a52903ce937f0d5fdca80802
CharlesWuChen <charles.wu.2000@gmail.com>
2016-08-02 17:50:27 +1000

    spittlecontroller with path variable

--- SpringInAction4/src/main/java/spittr/web/SpittleController.java
+++ SpringInAction4/src/main/java/spittr/web/SpittleController.java
@@ -4,15 +4,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import spittr.Spittle;
 import spittr.data.SpittleRepository;
 
 @Controller
 @RequestMapping("/spittles")
 public class SpittleController {
-	
+	private static final String MAX_LONG_AS_STRING = "" + Long.MAX_VALUE;
+
 	private SpittleRepository spittleRepository;
 
 	@Autowired
@@ -20,9 +24,36 @@ public class SpittleController {
 		this.spittleRepository = spittleRepository;
 	}
 
+	// @RequestMapping(method = RequestMethod.GET)
+	// public String spittles(Model model) {
+	// //default attribute name will be generated as spittleList
+	// model.addAttribute(spittleRepository.findSpittles(Long.MAX_VALUE, 20));
+	// return "spittles";
+	// }
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String spittles(Model model) {
-		model.addAttribute(spittleRepository.findSpittles(Long.MAX_VALUE, 20));
+	public String spittles(Model model,
+			@RequestParam(value = "max", required = false, defaultValue = MAX_LONG_AS_STRING) long max,
+			@RequestParam(value = "count", required = false, defaultValue = "20") int count) {
+
+		model.addAttribute(spittleRepository.findSpittles(max, count));
 		return "spittles";
+	}
+
+	@RequestMapping(value = "/show", method = RequestMethod.GET)
+	public String showSpittle(
+			@RequestParam("spittle_id") long spittleId, 
+			Model model) {
+		model.addAttribute(spittleRepository.findOne(spittleId));
+		return "spittle";
+
+	}
+	
+	@RequestMapping(value="/{spittleId}", method=RequestMethod.GET)
+	public String spittle(
+			@PathVariable("spittleId") long spittleId,
+			Model model){
+		model.addAttribute(spittleRepository.findOne(spittleId));
+		return "spittle";
 	}
 }
