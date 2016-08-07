90daddcffc30c1e92271df94f338cb1f6af2c9b4
CharlesWuChen <charles.wu.2000@gmail.com>
2016-08-07 21:37:37 +1000

    add chap 10 jdbc project

--- SpringInAction4/src/main/java/spittr/config/DataConfig.java
+++ SpringInAction4/src/main/java/spittr/config/DataConfig.java
@@ -8,21 +8,27 @@ import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jndi.JndiObjectFactoryBean;
 
 @Configuration
 public class DataConfig {
 
-  @Bean
-  public DataSource dataSource() {
-    return new EmbeddedDatabaseBuilder()
-            .setType(EmbeddedDatabaseType.H2)
-            .addScript("schema.sql")
-            .build();
-  }
-  
-  @Bean
-  public JdbcOperations jdbcTemplate(DataSource dataSource) {
-    return new JdbcTemplate(dataSource);
-  }
+	@Bean
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript("schema.sql").build();
+	}
 
+	@Bean
+	public JdbcOperations jdbcTemplate(DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
+	}
+
+	@Bean
+	public JndiObjectFactoryBean dataSourceJNDI() {
+		JndiObjectFactoryBean jndiObjectFB = new JndiObjectFactoryBean();
+		jndiObjectFB.setJndiName("jdbc/SpittrDS");
+		jndiObjectFB.setResourceRef(true);
+		jndiObjectFB.setProxyInterface(javax.sql.DataSource.class);
+		return jndiObjectFB;
+	}
 }
