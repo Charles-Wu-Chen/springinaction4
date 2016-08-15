355cc2b5f6f6e36a96a3c8130b3b7ce3e0ede62d
thinkerinhell <wuchen2010@gmail.com>
2016-08-15 23:18:29 +1000

    still error in main test

---
+++ Chapter_17_jms/jms/src/main/java/spittr/alerts/SpittleAlertHandler.java
@@ -0,0 +1,15 @@
+package spittr.alerts;
+
+import spittr.domain.Spittle;
+
+public class SpittleAlertHandler {
+
+//  public void handleSpittleAlert(Spittle spittle) {
+//    System.out.println(spittle.getMessage());
+//  }
+	
+  public void handleSpittleAlert(String spittle) {
+  System.out.println(spittle);
+}
+
+}
